64457b6fdd5b8d7327ab693714c3f26730a8ae86
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-09-13 15:16:33 +0700

    Update Feri.py

--- Feri.py
+++ Feri.py
@@ -242,7 +242,7 @@ async def alive(client: Client, message: Message):
     current_time = datetime.utcnow()
     uptime_sec = (current_time - START_TIME).total_seconds()
     uptime = await _human_time_duration(int(uptime_sec))
-    await message.reply_photo(
+    await message.reply_video(
         photo=f"{MEMEK}",
         caption=f"""**༄ Holla I'm [{KONTOL}](https://t.me/{BOT_USERNAME})**
 ༄ **I'm Working Properly**
