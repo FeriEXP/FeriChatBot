dc60800869c61b7703ce67b73d331f340cb8fae0
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-09-13 09:27:05 +0700

    Update Feri.py

--- Feri.py
+++ Feri.py
@@ -2,47 +2,58 @@ print("[INFO]: INITIALIZING ...")
 import re
 from time import time
 from datetime import datetime
-from asyncio import (gather, get_event_loop, sleep)
+from asyncio import gather, get_event_loop, sleep
 import requests
 from aiohttp import ClientSession
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram import (Client, filters, idle)
+from pyrogram import Client, filters, idle
 from Python_ARQ import ARQ
-from config import bot_token, ARQ_API_KEY, LANGUAGE, api_id, api_hash, ARQ_API_BASE_URL, BOT_USERNAME, KONTOL, MEMEK
-
+from config import (
+    bot_token,
+    ARQ_API_KEY,
+    LANGUAGE,
+    api_id,
+    api_hash,
+    ARQ_API_BASE_URL,
+    BOT_USERNAME,
+    KONTOL,
+    MEMEK,
+)
 
 print("[INFO]: INITIALIZING BOT CLIENT ...")
-luna = Client(":memory:",
-              bot_token=bot_token,
-              api_id=api_id,
-              api_hash=api_hash,
+feri = Client(
+    ":memory:",
+    bot_token=bot_token,
+    api_id=6,
+    api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e",
 )
+
 bot_id = int(bot_token.split(":")[0])
-print("[INFO]: INITIALIZING ...")
 arq = None
+print("[INFO]: INITIALIZING ...")
 
 
 START_TIME = datetime.utcnow()
 START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
 TIME_DURATION_UNITS = (
-    ('week', 60 * 60 * 24 * 7),
-    ('day', 60 * 60 * 24),
-    ('hour', 60 * 60),
-    ('min', 60),
-    ('sec', 1)
+    ("week", 60 * 60 * 24 * 7),
+    ("day", 60 * 60 * 24),
+    ("hour", 60 * 60),
+    ("min", 60),
+    ("sec", 1)
 )
 
 
 async def _human_time_duration(seconds):
     if seconds == 0:
-        return 'inf'
+        return "inf"
     parts = []
     for unit, div in TIME_DURATION_UNITS:
         amount, seconds = divmod(int(seconds), div)
         if amount > 0:
-            parts.append('{} {}{}'
+            parts.append("{} {}{}"
                          .format(amount, unit, "" if amount == 1 else "s"))
-    return ', '.join(parts)
+    return ", ".join(parts)
 
 
 async def lunaQuery(query: str, user_id: int):
@@ -76,14 +87,14 @@ async def type_and_send(message):
     else:
         responsess = responsee
     if "Who is feri?" in responsess:
-        responsess2 = responsess.replace("Who is feri?", "Nani?👀")
+        responsess2 = responsess.replace("Who is feri?", "Kontol bapak kau pecah")
     else:
         responsess2 = responsess
     await message.reply_text(responsess2)
     await message._client.send_chat_action(chat_id, "cancel")
 
 
-@luna.on_message(filters.command(["start", f"start@{BOT_USERNAME}"]) & ~filters.edited)
+@feri.on_message(filters.command(["start", f"start@{BOT_USERNAME}"]) & ~filters.edited)
 async def start(client: Client, message: Message):
     start = time()
     delta_ping = time() - start
@@ -98,10 +109,10 @@ async def start(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "Source", url="https://github.com/FeriEXP/FeriChatBot"
+                        "ꜱᴏᴜʀᴄᴇ", url="https://github.com/FeriEXP/FeriChatBot"
                     ),
                     InlineKeyboardButton(
-                        "GROUP", url="https://t.me/anossupport"
+                        "ꜱᴜᴘᴘᴏʀᴛ", url="https://t.me/anossupport"
                     )
                 ]
             ]
@@ -109,14 +120,14 @@ async def start(client: Client, message: Message):
     )
 
 
-@luna.on_message(
+@feri.on_message(
     ~filters.private
     & filters.text
-    & ~filters.command(["start", f"start@{BOT_USERNAME}"])
+    & ~filters.command(["help"])
     & ~filters.edited,
     group=69,
 )
-async def chat(_, message):
+async def chat(_, message: Message):
     if message.reply_to_message:
         if not message.reply_to_message.from_user:
             return
@@ -125,7 +136,7 @@ async def chat(_, message):
             return
     else:
         match = re.search(
-            f"[.|\n]{0,}feri[.|\n]{0,}",
+            "[.|\n]{0,}feri[.|\n]{0,}",
             message.text.strip(),
             flags=re.IGNORECASE,
         )
@@ -134,19 +145,7 @@ async def chat(_, message):
     await type_and_send(message)
 
 
-@luna.on_message(
-    filters.private
-    & ~filters.command(["start", f"start@{BOT_USERNAME}"])
-    & ~filters.edited
-)
-async def chatpm(_, message):
-    if not message.text:
-        await message.reply_text("Ufff... ignoring ....")
-        return
-    await type_and_send(message)
-
-
-@luna.on_message(filters.command(["asupan", f"asupan@{BOT_USERNAME}"]))
+@feri.on_message(filters.command(["asupan", f"asupan@{BOT_USERNAME}"]))
 async def asupan(client, message):
     try:
         resp = requests.get("https://api-tede.herokuapp.com/api/asupan/ptl").json()
@@ -156,7 +155,7 @@ async def asupan(client, message):
         await message.reply_text("`404 asupan videos not found:v`")
 
 
-@luna.on_message(filters.command(["wibu", f"wibu@{BOT_USERNAME}"]))
+@feri.on_message(filters.command(["wibu", f"wibu@{BOT_USERNAME}"]))
 async def wibu(client, message):
     try:
         resp = requests.get("https://api-tede.herokuapp.com/api/asupan/wibu").json()
@@ -166,7 +165,7 @@ async def wibu(client, message):
         await message.reply_text("`404 wibu not found:v`")
 
 
-@luna.on_message(filters.command(["truth", f"truth@{BOT_USERNAME}"]))
+@feri.on_message(filters.command(["truth", f"truth@{BOT_USERNAME}"]))
 async def truth(client, message):
     try:
         resp = requests.get("https://api-tede.herokuapp.com/api/truth").json()
@@ -176,7 +175,7 @@ async def truth(client, message):
         await message.reply_text("something went wrong...")
 
 
-@luna.on_message(filters.command(["dare", f"dare@{BOT_USERNAME}"]))
+@feri.on_message(filters.command(["dare", f"dare@{BOT_USERNAME}"]))
 async def dare(client, message):
     try:
         resp = requests.get("https://api-tede.herokuapp.com/api/dare").json()
@@ -186,7 +185,7 @@ async def dare(client, message):
         await message.reply_text("something went wrong...")
 
 
-@luna.on_message(filters.command(["chika", f"chika@{BOT_USERNAME}"]))
+@feri.on_message(filters.command(["chika", f"chika@{BOT_USERNAME}"]))
 async def chika(client, message):
     try:
         resp = requests.get("https://api-tede.herokuapp.com/api/chika").json()
@@ -201,7 +200,7 @@ async def main():
     session = ClientSession()
     arq = ARQ(ARQ_API_BASE_URL, ARQ_API_KEY, session)
 
-    await luna.start()
+    await feri.start()
     print(
         """
     -----------------
